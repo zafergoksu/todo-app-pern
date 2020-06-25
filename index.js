8ed661b8733121cb312357172a4a529a32bb2128
zgoksu <zafergoksu00@gmail.com>
2020-06-25 23:29:47 +0100

    Added build

--- index.js
+++ index.js
@@ -10,21 +10,19 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-        
-    });
-}
-
-
 //routes
 
 app.use("/auth", authRoutes);
 
 app.use("/users", dashboardRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        
+    });
+}
 
 app.listen(port, () => {
   console.log(`Server is starting on port 5000`);
